a19c541172ee19eaf0b160abab1845f8f9215fb8
JorgeM1110 <mejiapity404@gmail.com>
2025-08-04 22:15:29 -0700

    update app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layout/MainLayout';
 import HomePage from './pages/HomePage';
 import ResumeCheckerPage from './pages/ResumeCheckerPage';
-import DashboardPage from './pages/DashboardPage';
 import ChatbotPage from './pages/ChatBotPage';
 import CareerPathPage from './pages/CareerPathPage';
 
@@ -16,7 +15,6 @@ export default function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<HomePage />} /> 
           <Route path="resume-checker" element={<ResumeCheckerPage />} />
-          <Route path="dashboard" element={<DashboardPage />} /> 
           <Route path="chatbot" element={<ChatbotPage />} /> 
           <Route path="careerpath" element={<CareerPathPage />} /> 
         </Route>
